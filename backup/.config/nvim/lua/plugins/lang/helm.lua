deeb170d6860f5eac82bfb9f7dc405164f1c6ec8
divaltor <divaltor@pm.me>
2024-01-11 23:35:39 +0200

    Replace Lazy extras with explicit configs

---
+++ backup/.config/nvim/lua/plugins/lang/helm.lua
@@ -0,0 +1,26 @@
+if true then
+  return {}
+end
+
+require("lazyvim.util.lsp").on_attach(function(client, buffer)
+  if client.name == "docker_compose_language_service" then
+    if vim.api.nvim_buf_get_option(buffer, "filetype") == "helm" then
+      vim.schedule(function()
+        vim.cmd("LspStop ++force docker_compose_language_service")
+      end)
+    end
+  end
+end)
+
+return {
+  "towolf/vim-helm",
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        docker_compose_language_service = {},
+        helm_ls = {},
+      },
+    },
+  },
+}
