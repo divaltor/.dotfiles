1548a664702313de87eae04f1074fa292832e560
Vlad Vladov <divaltor@pm.me>
2024-08-26 23:40:32 +0300

    Update configs

--- backup/.config/nvim/lua/plugins/lang/python.lua
+++ backup/.config/nvim/lua/plugins/lang/python.lua
@@ -16,6 +16,9 @@ return {
               },
             },
           },
+          on_init = function(client, _)
+            client.server_capabilities.semanticTokensProvider = nil
+          end,
         },
       },
     },
