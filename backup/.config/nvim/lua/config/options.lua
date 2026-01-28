64af34ac5b8fe2ab62add7bebde15d544720b235
Vlad Vladov <divaltor@pm.me>
2026-01-28 16:10:09 +0200

    Replace `vtsls` with `tsgo` and `basedpyright` with `ty`

--- backup/.config/nvim/lua/config/options.lua
+++ backup/.config/nvim/lua/config/options.lua
@@ -2,6 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazygit_config = false
-vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_lsp = "ty"
 vim.g.lazyvim_python_ruff = "ruff"
 vim.g.snacks_animate = false
---
+++ backup/.config/nvim/lua/plugins/lang/typescript.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vtsls = {
+          enabled = false,
+        },
+        tsgo = {},
+      },
+    },
+  },
+}
