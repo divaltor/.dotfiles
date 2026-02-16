d39076fdb4fee436a72643da964dd82ecd385b42
Vlad Vladov <divaltor@pm.me>
2026-02-16 22:00:46 +0200

    Remove recent files from dashboard

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -8,7 +8,6 @@ return {
           keys = {
             { icon = "󰈞 ", key = "f", desc = "Find File", action = function() require("fff").find_in_git_root() end },
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "r", desc = "Recent Files", action = function() Snacks.picker.recent() end },
             { icon = " ", key = "g", desc = "Find Text", action = function() require("fff").live_grep() end },
             { icon = " ", key = "c", desc = "Config", action = function() require("fff").find_files_in_dir(vim.fn.stdpath("config")) end },
             { icon = " ", key = "s", desc = "Restore Session", action = function() require("persistence").load() end },
