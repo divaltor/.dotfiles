f78d87a9eac47c3061933837f614d846d3ca18e0
Vlad Vladov <divaltor@pm.me>
2026-02-26 21:28:42 +0200

    Add oxlint and oxfmt

--- .config/nvim/lua/plugins/lang/typescript.lua
+++ .config/nvim/lua/plugins/lang/typescript.lua
@@ -7,6 +7,20 @@ return {
           enabled = false,
         },
         tsgo = {},
+        oxlint = {},
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "oxfmt" },
+        javascriptreact = { "oxfmt" },
+        typescript = { "oxfmt" },
+        typescriptreact = { "oxfmt" },
+        json = { "oxfmt" },
+        vue = { "oxfmt" },
       },
     },
   },
