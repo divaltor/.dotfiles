1cd4788e38231d221c5fcedaae50b24c5a091488
Vlad Vladov <divaltor@pm.me>
2026-07-10 12:44:34 +0300

    Update injections for Python

--- .config/nvim/after/queries/python/injections.scm
+++ .config/nvim/after/queries/python/injections.scm
@@ -1,70 +1,88 @@
 ;extends
 
 ; query
-;; comment sql injection
-((comment) @comment .
-           (expression_statement
-             (assignment right: 
-                         (string
-                           (string_content)
-                           @injection.content 
-                           (#match? @comment "^#+( )*[sS][qQ][lL]( )*") 
-                           (#set! injection.language "sql")))))
+;; generic comment-based language injection
+;; place a comment like `# sql`, `# javascript`, `# html` on the line above the string.
+;; the comment text (minus `#`) becomes the injection language name.
+;; language names must be lowercase (neovim tree-sitter parser names are lowercase).
+;; examples: `# sql`, `# javascript`, `# typescript`, `# html`, `# css`, `# python`
+((comment) @injection.language
+  (#lua-match? @injection.language "^#%s*%a+%s*$")
+  (#gsub! @injection.language "^#%s*(%a+)%s*$" "%1")
+  .
+  (expression_statement
+    (assignment
+      right: (string
+        (string_content) @injection.content))))
 
 ; query
-;; comment javascript injection
-((comment) @comment .
-           (expression_statement
-             (assignment right: 
-                         (string
-                           (string_content)
-                           @injection.content 
-                           (#match? @comment "^#+( )*[jJ][aA][vV][aA][sS][cC][rR][iI][pP][tT]( )*") 
-                           (#set! injection.language "javascript")))))
+;; variable-name suffix injection (no comment needed)
+;; matches UPPER_CASE constants whose name ends in a language suffix.
+;; rename e.g. `DETECT_CREATIVE_TYPE_SCRIPT` -> `DETECT_CREATIVE_TYPE_JS` to use this.
 
-; query
-;; comment typescript injection
-((comment) @comment .
-           (expression_statement
-             (assignment right: 
-                         (string
-                           (string_content)
-                           @injection.content 
-                           (#match? @comment "^#+( )*[tT][yY][pP][eE][sS][cC][rR][iI][pP][tT]( )*") 
-                           (#set! injection.language "typescript")))))
+((expression_statement
+  (assignment
+    left: (identifier) @_var
+    right: (string
+      (string_content) @injection.content)))
+  (#lua-match? @_var "_SQL$")
+  (#set! injection.language "sql"))
 
-; query
-;; comment html injection
-((comment) @comment .
-           (expression_statement
-             (assignment right: 
-                         (string
-                           (string_content)
-                           @injection.content 
-                           (#match? @comment "^#+( )*[hH][tT][mM][lL]( )*") 
-                           (#set! injection.language "html")))))
+((expression_statement
+  (assignment
+    left: (identifier) @_var
+    right: (string
+      (string_content) @injection.content)))
+  (#lua-match? @_var "_JAVASCRIPT$")
+  (#set! injection.language "javascript"))
 
-; query
-;; comment css injection
-((comment) @comment .
-           (expression_statement
-             (assignment right: 
-                         (string
-                           (string_content)
-                           @injection.content 
-                           (#match? @comment "^#+( )*[cC][sS][sS]( )*") 
-                           (#set! injection.language "css")))))
+((expression_statement
+  (assignment
+    left: (identifier) @_var
+    right: (string
+      (string_content) @injection.content)))
+  (#lua-match? @_var "_JS$")
+  (#set! injection.language "javascript"))
 
-; query
-;; comment python injection
-((comment) @comment .
-           (expression_statement
-             (assignment right: 
-                         (string
-                           (string_content)
-                           @injection.content 
-                           (#match? @comment "^#+( )*[pP][yY][tT][hH][oO][nN]( )*") 
-                           (#set! injection.language "python")))))
+((expression_statement
+  (assignment
+    left: (identifier) @_var
+    right: (string
+      (string_content) @injection.content)))
+  (#lua-match? @_var "_TYPESCRIPT$")
+  (#set! injection.language "typescript"))
+
+((expression_statement
+  (assignment
+    left: (identifier) @_var
+    right: (string
+      (string_content) @injection.content)))
+  (#lua-match? @_var "_TS$")
+  (#set! injection.language "typescript"))
+
+((expression_statement
+  (assignment
+    left: (identifier) @_var
+    right: (string
+      (string_content) @injection.content)))
+  (#lua-match? @_var "_HTML$")
+  (#set! injection.language "html"))
+
+((expression_statement
+  (assignment
+    left: (identifier) @_var
+    right: (string
+      (string_content) @injection.content)))
+  (#lua-match? @_var "_CSS$")
+  (#set! injection.language "css"))
+
+((expression_statement
+  (assignment
+    left: (identifier) @_var
+    right: (string
+      (string_content) @injection.content)))
+  (#lua-match? @_var "_PYTHON$")
+  (#set! injection.language "python"))
 
 ; query
 ;; string sql injection
